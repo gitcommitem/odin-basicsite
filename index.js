a796210deb5af155ea8c43933862263202bfa570
Em <contactemilyhuang@gmail.com>
2022-06-02 20:02:43 -0700

    Add function that routes pages

--- index.js
+++ index.js
@@ -6,11 +6,35 @@ const host = 'localhost'
 const port = 8080
 
 const requestListener = (req, res) => {
-  fs.readFile(__dirname + '/index.html').then(contents => {
-    res.setHeader('content-type', 'text/html')
-    res.writeHead(200)
-    res.end(contents)
-  })
+  switch (req.url) {
+    case '/':
+      fs.readFile(__dirname + '/index.html').then(contents => {
+        res.setHeader('content-type', 'text/html')
+        res.writeHead(200)
+        res.end(contents)
+      })
+      break
+    case '/about':
+      fs.readFile(__dirname + '/about.html').then(contents => {
+        res.setHeader('content-type', 'text/html')
+        res.writeHead(200)
+        res.end(contents)
+      })
+      break
+    case '/contact-me':
+      fs.readFile(__dirname + '/contact-me.html').then(contents => {
+        res.setHeader('content-type', 'text/html')
+        res.writeHead(200)
+        res.end(contents)
+      })
+      break
+    default:
+      fs.readFile(__dirname + '/404.html').then(contents => {
+        res.setHeader('content-type', 'text/html')
+        res.writeHead(404)
+        res.end(contents)
+      })
+  }
 }
 
 const server = http.createServer(requestListener)
